88b088a3392556a695aa706ee7ab60850305c5c6
Matias <mhumpirig@unjbg.edu.pe>
2025-05-29 21:52:33 -0500

    Ahora son los multiplos de 9

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 int main (){
 	int n;
-	cout << "Ingrese la cantidad de multiplos de 7: ";
+	cout << "Ingrese la cantidad de multiplos de 9: ";
 	cin >> n;
 	for (int i = 1; i <= n; i++){
 		if (i < n){
-		cout << i * 7 << " , ";
+		cout << i * 9 << " , ";
 	} else {
-		cout << i*7;
+		cout << i*9;
 	}
 	}
 	return 0;
