759c6b0b397fdfd48ad9f4dfaa849ad8db5e326e
Matias <mhumpirig@unjbg.edu.pe>
2025-05-30 22:47:02 -0500

    Agregando Ejercicio3.cpp

---
+++ Ejercicio3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main (){
+	int n, suma;
+	cout << "Indique los 'n' digitos a sumar: ";
+	cin >> n;
+	suma = 0;
+	for (int i = 1; i <= n; i++ ){
+		suma = suma + i;
+}
+	cout << suma;
+return 0;
+}
