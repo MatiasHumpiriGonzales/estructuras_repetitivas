26777cdda45984820f1dd9b7ef88f1dcb7b3a0bc
Matias <mhumpirig@unjbg.edu.pe>
2025-06-02 10:02:29 -0500

    Ahora hace restas sucesivas para dividir

--- Ejercicio13.cpp
+++ Ejercicio13.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 using namespace std;
-int main(){
-	int a, b, m;
-	m = 0;
-	cout << "Ingrese el multiplicando: ";
-	cin >> a;
-	cout << "Ingrese el multiplicador: ";
-	cin >> b;
-	for (int i = 0; i < b; i++){
-		m = m + a;
-	}
-	cout << "La multiplicacion es : " << m;
-	return 0;
+int main() {
+    int dividendo, divisor;
+    int cociente = 0;
+    cout << "Ingresa el dividendo: ";
+    cin >> dividendo;
+    cout << "Ingresa el divisor: ";
+    cin >> divisor;
+    if (divisor <= 0) {
+        cout << "El divisor debe ser mayor que cero." << endl;
+        return 1;
+    }
+    int restante = dividendo;
+    while (restante >= divisor) {
+        restante -= divisor;
+        cociente++;
+    }
+    cout << "Cociente: " << cociente << endl;
+    cout << "Residuo: " << restante << endl;
+    return 0;
 }
+
